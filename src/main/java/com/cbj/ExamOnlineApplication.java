0c2ef53f96b97138c804b406f8998a4ea4402c63
home\john <785562850@qq.com>
2018-09-18 00:07:41 +0800

    基础服务

---
+++ src/main/java/com/cbj/ExamOnlineApplication.java
@@ -0,0 +1,12 @@
+package com.cbj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamOnlineApplication.class, args);
+	}
+}
